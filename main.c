941176364b8911305c3539fb9b1b306eaf40583d
JP113z <josepablof113@gmail.com>
2025-08-21 15:52:14 -0600

    Makefile y main completos

--- main.c
+++ main.c
@@ -1,34 +1,36 @@
 #include <gtk/gtk.h>
-#include <gtk/gtkx.h>
 #include <stdio.h>
 
-void on_btn_clicked(GtkWidget *GtkWidget, gpointer d){
-  system("./pending &");
+void on_btn_clicked(GtkWidget *GtkWidget, gpointer d) {
+    system("./pending &");
 }
 
-
-void on_btn_salir_clicked(GtkWidget *GtkWidget, gpointer d) {
+void on_btn_exit_clicked(GtkWidget *GtkWidget, gpointer d) {
     gtk_main_quit();
 }
 
 int main(int argc, char *argv[]) {
-  GtkWidget *window;
-  GtkWidget *label1;
-  GtkBuilder *builder;
-  gtk_init(&argc, &argv);
+    GtkWidget *window;
+    GtkWidget *label1;
+    GtkBuilder *builder;
+    gtk_init(&argc, &argv);
 
-  builder = gtk_builder_new_from_file ("main.glade");
-  window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
-  
-  gtk_builder_add_callback_symbols(builder, "on_btn_salir_clicked", G_CALLBACK(on_btn_salir_clicked), "on_btn_clicked", G_CALLBACK(on_btn_clicked), NULL);
-  gtk_builder_connect_signals(builder, NULL);
-  
-  label1 = GTK_WIDGET(gtk_builder_get_object(builder, "label1"));
-  
-  gtk_widget_show(window  );
-  gtk_main();
-  return EXIT_SUCCESS;
+    builder = gtk_builder_new_from_file("main.glade");
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
+
+    gtk_builder_add_callback_symbols(
+        builder,
+        "on_btn_exit_clicked", G_CALLBACK(on_btn_exit_clicked),
+        "on_btn_clicked",      G_CALLBACK(on_btn_clicked),
+        NULL
+    );
+
+    gtk_builder_connect_signals(builder, NULL);
+
+    label1 = GTK_WIDGET(gtk_builder_get_object(builder, "label1"));
+
+    gtk_widget_show(window);
+    gtk_main();
+    return EXIT_SUCCESS;
 }
-
-
 
